83c3ff858680cf030d6d22b407cc97fdefcc9639
clamp2x <clamp2x@hotmail.com>
2020-10-26 00:31:31 +0900

    Update ad_insert.class.php
    초기 세팅

--- ad_insert.class.php
+++ ad_insert.class.php
@@ -1,1 +1,31 @@
+<?php
 
+    class ad_insert extends WidgetHandler {
+
+        /**
+         * @brief 위젯의 실행 부분
+         *
+         * ./widgets/위젯/conf/info.xml 에 선언한 extra_vars를 args로 받는다
+         * 결과를 만든후 print가 아니라 return 해주어야 한다
+         **/
+        function proc($args) {
+            # 로그인 정보
+            if(!Context::get('is_logged')) return;
+
+            $logged_info = Context::get('logged_info');
+            $member_srl = $logged_info->member_srl;
+            if(!$member_srl) return;
+
+            #템플릿 설정
+            Context::set('widget_info', $widget_info);
+		
+            $tpl_path = sprintf('%sskins/%s', $this->widget_path, $args->skin);
+            Context::set('colorset', $args->colorset);
+
+            $tpl_file = 'ad_insert';
+
+            $oTemplate = &TemplateHandler::getInstance();
+            return $oTemplate->compile($tpl_path, $tpl_file);
+        }
+    }
+?>
